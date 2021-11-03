b56a795901813eccf659cd6dab3729d6893e7bed
Denis Gusar <degusar.wot@gmail.com>
2021-11-03 20:48:16 +0300

    feat: finish 02-write-file task

---
+++ 02-write-file/index.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const path = require('path');
+const readline = require('readline');
+const { stdin, stdout } = process;
+let filePath = path.join(__dirname, 'write.txt');
+const newText = fs.createWriteStream(filePath, 'utf-8');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+  
+stdout.write('Privet! Write some text...\n');
+
+rl.on('line', line => {
+    if (line === 'exit') {
+        rl.close()
+    }
+    else  newText.write(line)
+})
+
+process.on('exit', () => stdout.write('Удачи в изучении Node.js!'));
