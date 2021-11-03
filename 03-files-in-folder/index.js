d36e07153a1c79c654e5bf68bc8ba9a1555bdd14
Denis Gusar <degusar.wot@gmail.com>
2021-11-04 00:03:12 +0300

    feat: finish 03-files-in-folder task

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const path = require('path');
+
+pathFolder = path.join(__dirname, 'secret-folder');
+fs.promises.readdir(pathFolder, {withFileTypes: true}).then(filenames => {
+    filenames.forEach(item => {
+        if (item.isFile()) {
+           let dividedName =  item.name.split('.')
+            fs.promises.stat(path.join(pathFolder, item.name)).then(stats => {
+               console.log(`${dividedName[0]} - ${dividedName[1]} - ${stats.size}b`)
+           }) 
+     }
+  })
+})
+  
