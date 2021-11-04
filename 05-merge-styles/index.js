9c79cef4d56278b7db1d1eeb46473523885add58
Denis Gusar <degusar.wot@gmail.com>
2021-11-04 17:26:51 +0300

    feat:finish 05-merge-styles task

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const path = require('path');
+
+let pathStyles = path.join(__dirname, 'styles')
+let pathDist = path.join(__dirname, 'project-dist')
+fs.promises.readdir(pathDist).then(data => {
+    data.forEach(bundle => {
+        if (bundle == 'bundle.css') {
+            fs.promises.writeFile(path.join(pathDist,bundle), '')
+           
+        }
+    })
+})
+fs.promises.readdir(pathStyles).then(styles => {
+    styles.forEach(style => {
+        if (path.extname(style) == '.css') {
+            readableStream = fs.createReadStream(path.join(pathStyles, style), "utf8");
+        readableStream.on('data', data => {
+            fs.promises.appendFile(path.join(pathDist, 'bundle.css'), data)
+        });
+        }
+        
+       
+    })
+   
+})
