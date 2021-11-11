1fe69f9419cc2d483effaf1e5f6a599fcfaf4d76
Denis Gusar <degusar.wot@gmail.com>
2021-11-11 21:03:06 +0300

    fix: change 03-files-in-folder task

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -7,7 +7,7 @@ fs.promises.readdir(pathFolder, {withFileTypes: true}).then(filenames => {
         if (item.isFile()) {
            let dividedName =  item.name.split('.')
             fs.promises.stat(path.join(pathFolder, item.name)).then(stats => {
-               console.log(`${dividedName[0]} - ${dividedName[1]} - ${stats.size}b`)
+                console.log(`${dividedName[0]} - ${dividedName[dividedName.length - 1]} - ${stats.size}b`);
            }) 
      }
   })
